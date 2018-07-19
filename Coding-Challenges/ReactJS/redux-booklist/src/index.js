f521657001db35492d17488a4240b9fb4449597b
Christopher Beards <beardscm@gmail.com>
2018-07-18 21:01:20 -0400

    Book-List; Dev Tools Added

--- Coding-Challenges/ReactJS/redux-booklist/src/index.js
+++ Coding-Challenges/ReactJS/redux-booklist/src/index.js
@@ -7,7 +7,10 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducers from './reducers';
 
-const store = createStore(rootReducers);
+const store = createStore(
+  rootReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 ReactDOM.render(
   <Provider store={store}>
