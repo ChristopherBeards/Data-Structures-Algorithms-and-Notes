aa52c90ae2b987f79a8ef7ae9a70008ad7d3b547
Christopher Beards <beardscm@gmail.com>
2018-08-07 18:28:13 -0400

    Minor

--- Coding-Challenges/JavaScript/findVowels/index.js
+++ Coding-Challenges/JavaScript/findVowels/index.js
@@ -6,12 +6,6 @@ findVowels('aloha'); // -> 3
 Testing: `jest findVowels/test.js`
 */
 
-// function findVowels(str) {
-//   const vowels = 'aeiou';
-//   if (!str.length) return 0;
-//   return (vowels.includes(str[0]) ? 1 : 0) + findVowels(str.slice(1));
-// }
-
 function findVowels(str) {}
 
 module.exports = findVowels;
