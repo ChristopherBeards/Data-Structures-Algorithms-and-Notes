3e061b537c56495ce498c64dba07a577932b9211
Christopher Beards <BeardsCM@gmail.com>
2018-10-13 23:04:04 -0400

    Stack Scaffolding Added

---
+++ Data-Structures/Stack/index.js
@@ -0,0 +1,19 @@
+/* Stack
+Create a stack data structure.  The stack
+should be a class with methods 'push', 'pop', and
+'peek'.  Adding an element to the stack should
+store it until it is removed.
+
+Examples:
+   const s = new Stack();
+   s.push(1);
+   s.push(2);
+   s.pop(); // returns 2
+   s.pop(); // returns 1
+
+Testing: `jest Stack/test.js`
+*/
+
+class Stack {};
+
+module.exports = Stack;
