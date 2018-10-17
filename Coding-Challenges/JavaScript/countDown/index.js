e97b2c9680dd016365c87e12ad05c40a09232203
Christopher Beards <BeardsCM@gmail.com>
2018-10-17 15:07:01 -0400

    countDown; challenge added

---
+++ Coding-Challenges/JavaScript/countDown/index.js
@@ -0,0 +1,12 @@
+/* Count Down
+Without changing the _starting_ value of `i`
+create a function that returns an array of numbers
+from 100 to 1 in descending order. 
+Testing: `jest countDown/test.js`
+*/
+
+const countDown = () => {
+  for (let i = 0; i; i) {}
+};
+
+module.exports = countDown;
