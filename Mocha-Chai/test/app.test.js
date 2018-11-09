0da696f9c8231288dbf32fea9b8e0c2e60e2ced4
Christopher Beards <BeardsCM@gmail.com>
2018-11-09 14:58:55 -0500

    Mocha-Chai; introduction notes

--- Mocha-Chai/test/app.test.js
+++ Mocha-Chai/test/app.test.js
@@ -3,8 +3,14 @@ const assert = require('chai').assert;
 const { helloWorld } = require('../app');
 
 // Creates a test
-describe('App', function() {
-  it('helloWorld should return properly', function() {
-    assert.equal(helloWorld(), 'Hello World!');
+describe('App', () => {
+  it('helloWorld should return properly', () => {
+    let result = helloWorld();
+    assert.equal(result, 'Hello World!');
+  });
+
+  it('helloWorld should return a string', () => {
+    let result = helloWorld();
+    assert.typeOf(result, 'string');
   });
 });
